4300e2b5504301581f84a825f11e93ed513f85b2
antioy <71178705+antioy@users.noreply.github.com>
2023-07-31 16:29:17 +0300

    Homework02

---
+++ Task05More.java
@@ -0,0 +1,32 @@
+package Homework02;
+
+import java.util.Arrays;
+
+public class Task05More {
+    public static void main(String[] args)
+    {
+        // Custom input array
+        int a[] = { 10, 12, 1, 8, 4 };
+
+        // Create an array b[] of same size as a[]
+        // Copy elements of a[] to b[]
+        int b[] = Arrays.copyOf(a, 5);
+
+        // Change b[] to verify that
+        // b[] is different from a[]
+        b[0]++;
+
+        System.out.println("Contents of a[] ");
+
+        // Iterating over array. a[]
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
+
+        System.out.println("\n\nContents of b[] ");
+
+        // Iterating over array b[]
+        for (int i = 0; i < b.length; i++)
+            System.out.print(b[i] + " ");
+    }
+}
+}
