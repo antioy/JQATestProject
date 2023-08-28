0a6c340e912d26498bca2dd1f564385bc67fb999
antioy <tsankovatony@gmail.com>
2023-08-28 17:06:25 +0300

    test

--- src/main/java/Homework06/Cat.java
+++ src/main/java/Homework06/Cat.java
@@ -7,7 +7,7 @@ public  class Cat extends AbstractAnimal {
 
         @Override
         public void makeSomeNoise () {
-            System.out.println("Meow!");
+            System.out.println("Meowo!");
         }
     }
 }
